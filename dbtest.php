6951b9c100a7a997dcac659289183f54cb1d4742
cfbianchi <cfbianchi@hotmail.com>
2019-05-31 17:58:49 -0300

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,7 +5,11 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
+/*
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+*/
+$connectionInfo = array( $dbname, $dbuser, $dbpwd );
+$connection = sqlsrv_connect( $dbhost, $connectionInfo);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
